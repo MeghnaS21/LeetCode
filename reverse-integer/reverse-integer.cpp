dd177dc7bc73c3af9c57d45eb1e51afe8ce0bd80
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2021-09-21 10:06:33 +0530

    Time: 4 ms (40.29%), Space: 5.9 MB (77.04%) - LeetHub

---
+++ reverse-integer/reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int ans =0;
+        while(x!=0){
+            if(ans>INT_MAX/10 || ans < INT_MIN/10)
+                return 0;
+            ans = ans*10 + x%10;
+            x = x/10;
+        }
+        
+        return ans;
+        
+    }
+};
