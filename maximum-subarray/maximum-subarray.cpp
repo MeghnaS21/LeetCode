0d6156506b9eb05a7c5559836a16687ffbcddca0
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2021-10-15 09:32:51 +0530

    Time: 201 ms (5.88%), Space: 67.8 MB (21.54%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum=0;
+        int maxi = INT_MIN;
+        
+        for(auto it: nums){
+            sum += it;
+            maxi = max(maxi, sum);
+            if(sum<0) sum=0;
+        }
+        return maxi;
+        
+    }
+};
