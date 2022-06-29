b8efb9d017b1e824b11e7beb844034aa5ab482ce
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2022-06-29 12:00:37 +0530

    Time: 7 ms, Memory: 9.9 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -3,10 +3,15 @@ public:
     bool checkIfExist(vector<int>& arr) {
         for(int i = 0; i < arr.size(); i++)
         {
-            auto it = find(arr.begin(), arr.end(), arr[i]*2);
-            if(it != arr.end() && it != arr.begin()+i)
-            {
-                return true;
+            if(arr[i]%2==0){
+                int j=0;
+                while(j<arr.size()){
+                    if(arr[j]==arr[i]/2 && j!=i)
+                    {
+                        return true;
+                    }
+                    ++j;
+                }
             }
         }
         return false;
