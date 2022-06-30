ecaafdb10a6c9ec265044674d78f69a4253aa8f6
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2022-06-30 19:42:24 +0530

    Time: 22 ms, Memory: 16.3 MB - LeetHub

--- sort-array-by-parity/sort-array-by-parity.cpp
+++ sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& A) {
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        
+        int first =0;
+        int last = nums.size()-1;
+        while(first<last){
+            if(nums[first]%2!=0 && nums[last]%2==0){
+                swap(nums[first], nums[last]);
+                  first++;
+                  last--;
+            }
+            if(nums[first]%2==0)
+                first++;
+            if(nums[last]%2!=0)
+                last--;
+            
+        }
+        return nums;
+        
+        /*
         int start = 0, end = A.size()-1;
 
-      while(start<end){
-          if(A[start]%2!=0 && A[end]%2==0){
-              swap(A[start],A[end]);
-              start++;
-              end--;
+          while(start<end){
+              if(A[start]%2!=0 && A[end]%2==0){
+                  swap(A[start],A[end]);
+                  start++;
+                  end--;
+              }
+
+              if(A[start]%2 == 0){
+                  start++;
+              }
+
+              if(A[end]%2 != 0){
+                  end--;
+              }
           }
 
-          if(A[start]%2 == 0){
-              start++;
-          }
-
-          if(A[end]%2 != 0){
-              end--;
-          }
-      }
-
-    return A;
-   }
+          return A;
+        */
+    }
 };
