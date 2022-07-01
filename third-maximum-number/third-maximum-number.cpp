276ba055796eb4bf8e5da30944ab6aab964511ba
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2022-07-01 16:32:44 +0530

    Time: 0 ms, Memory: 9.2 MB - LeetHub

---
+++ third-maximum-number/third-maximum-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        
+        long max1 = LONG_MIN; 
+        long max2 = LONG_MIN;  
+        long max3 = LONG_MIN;  
+
+        for (const int num : nums)
+          if (num > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = num;
+          } else if (max1 > num && num > max2) {
+            max3 = max2;
+            max2 = num;
+          } else if (max2 > num && num > max3) {
+            max3 = num;
+          }
+
+        return max3 == LONG_MIN ? max1 : max3;
+        
+    }
+};
