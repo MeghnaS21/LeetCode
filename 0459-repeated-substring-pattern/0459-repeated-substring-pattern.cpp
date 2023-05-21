a97fd768b1d413eb4a70669e0ce0bcfd83493c38
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2023-05-21 23:30:04 +0530

    Time: 24 ms (42.13%), Space: 13.5 MB (57.48%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        string t=s+s;
-        if(t.substr(1,t.size()-2).find(s)!=-1)
+        string t=s+s; //adding the same string twice in a different string t
+        if(t.substr(1,t.size()-2).find(s)!=-1) //checking the subsstring
             return true;
         return false;
     }
