3cac5cc9a88f731b8bb2bfade9a84584887aef0f
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2023-05-25 22:40:35 +0530

    Time: 3 ms (55.81%), Space: 8.2 MB (81.27%) - LeetHub

---
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string tictactoe(vector<vector<int>>& moves) {
+        vector<int> A(8,0), B(8,0); // 3 rows, 3 cols, 2 diagonals
+      for(int i=0; i<moves.size(); i++) {
+          int r=moves[i][0], c=moves[i][1];
+          vector<int>& player = (i%2==0)?A:B;
+          player[r]++;
+          player[c+3]++; 
+          if(r==c) player[6]++;
+          if(r==2-c) player[7]++;
+      }
+      for(int i=0; i<8; i++) {
+          if(A[i]==3) return "A";
+          if(B[i]==3) return "B";
+      }
+      return moves.size()==9 ? "Draw":"Pending";
+    }
+};
